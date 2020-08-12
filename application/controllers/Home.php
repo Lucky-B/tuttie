9878b9d084fc3287ac1f41d743e61c58e4c4df67
Lucky <luckybogatsu@gmail.com>
2020-08-12 16:55:55 +0200

    Frame work downgrade to CI3 , CI4 is erratic

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function __construct() 
+	{
+		parent::__construct();
+	//	$this->load->helper('form');
+	//	$this->load->library('form_validation');
+	//	$this->load->library('session');
+		$this->load->helper('url');
+	}
+	
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('template/index');
+		$this->load->view('template/footer');
+	}
+}
